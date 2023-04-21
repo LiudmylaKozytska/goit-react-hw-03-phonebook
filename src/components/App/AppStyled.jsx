5d3d9c45a268f2fe28412d72d0f7e6089a5c15e4
LiudmylaKozytska <ludaanishchenko0512@gmail.com>
2023-04-21 14:44:45 +0200

    update

--- src/components/App/AppStyled.jsx
+++ src/components/App/AppStyled.jsx
@@ -4,8 +4,6 @@ export const Container = styled.div`
   display: flex;
   gap: 24px;
   padding: 60px;
-  background-color: #d9afd9;
-  background-image: linear-gradient(59deg, #d9afd9 0%, #97d9e1 100%);
   text-align: center;
 `;
 
